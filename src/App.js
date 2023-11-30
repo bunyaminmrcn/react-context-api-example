02ec24f3dd0da90484cc94479fc72c0541e16d89
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-11-30 20:42:11 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+
+    }
+}
+
+
+export default App;
